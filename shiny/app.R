cd67ff075626e6cb842392bf3f0d199c5dd5d94a
lrcarey222 <lcarey@rmi.org>
2026-01-28 11:48:13 -0500

    1/28/26

--- shiny/app.R
+++ shiny/app.R
@@ -240,7 +240,7 @@ server <- function(input, output, session) {
   })
 
   shiny::observeEvent(input$dw_update, {
-    api_key <- Sys.getenv("DATAWRAPPER_API_KEY", "")
+    api_key <- Sys.getenv("DATAWRAPPER_API_KEY")
     if (!nzchar(api_key)) {
       dw_status("Datawrapper disabled. Set DATAWRAPPER_API_KEY to enable publishing.")
       dw_iframe_src(NULL)
