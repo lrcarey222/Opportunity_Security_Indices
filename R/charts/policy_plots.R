be76c4e4318897f336a155536abee784efb84a59
lrcarey222 <lcarey@rmi.org>
2026-02-06 11:13:27 -0500

    2/6/26

---
+++ R/charts/policy_plots.R
@@ -0,0 +1,26 @@
+
+
+processed_dir <- file.path(repo_root, config$processed_dir)
+if (!dir.exists(processed_dir)) {
+  stop("Processed data directory not found: ", processed_dir)
+}
+
+read_processed_tbl <- function(name, processed_dir) {
+  path <- file.path(processed_dir, paste0(name, ".rds"))
+  if (!file.exists(path)) {
+    stop("Processed table not found: ", path)
+  }
+  readRDS(path)
+}
+
+nipo_policy_index_tbl <- read_processed_tbl("nipo_policy_index_tbl", processed_dir)
+
+#Asia Trip NIPO Comparison
+
+asia_nipo <- nipo_policy_index_tbl %>%
+  filter(iso3 %in% c("JPN",
+                    "KOR",
+                    "VNM",
+                    "IND")) %>%
+  mutate(industry=paste0(tech,"-",supply_chain)) %>%
+  select(country, industry, domestic_strength)
