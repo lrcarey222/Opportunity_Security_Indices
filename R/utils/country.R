79ff995ad03133994967d752e0115fd2b4321035
lrcarey222 <110408824+lrcarey222@users.noreply.github.com>
2026-02-24 19:47:10 -0500

    Remove leaked helper columns from standardized theme tables

--- R/utils/country.R
+++ R/utils/country.R
@@ -10,7 +10,6 @@ country_recode_map <- function() {
     "Korea, Rep." = "South Korea",
     "Republic of Korea" = "South Korea",
     "Korea, Dem. People's Rep." = "North Korea",
-    "Vietnam" = "Viet Nam",
     "Iran, Islamic Rep." = "Iran",
     "Turkey" = "Turkiye",
     "Curacao" = "Curaçao",
@@ -67,6 +66,14 @@ standardize_country_table <- function(tbl, country_info = NULL) {
   }
 
   standardized <- tbl %>%
+    dplyr::select(-dplyr::any_of(c(
+      "country",
+      "country_std",
+      "country_std.x",
+      "country_std.y",
+      "iso3c.x",
+      "iso3c.y"
+    ))) %>%
     dplyr::mutate(
       Country = standardize_country_names(Country),
       iso3c = if ("iso3c" %in% names(tbl)) toupper(as.character(iso3c)) else NA_character_
@@ -78,21 +85,40 @@ standardize_country_table <- function(tbl, country_info = NULL) {
 
   country_ref <- country_info %>%
     dplyr::transmute(
-      iso3c = toupper(as.character(iso3c)),
-      country = as.character(country)
+      ref_iso3c = toupper(as.character(iso3c)),
+      ref_country = as.character(country),
+      ref_country_std = standardize_country_names(country)
     ) %>%
-    dplyr::distinct(iso3c, .keep_all = TRUE)
+    dplyr::distinct(ref_iso3c, .keep_all = TRUE)
 
   with_iso <- standardized %>%
     dplyr::filter(!is.na(iso3c), nzchar(iso3c)) %>%
-    dplyr::left_join(country_ref, by = "iso3c") %>%
-    dplyr::mutate(Country = dplyr::coalesce(country, Country)) %>%
-    dplyr::select(-country)
+    dplyr::left_join(country_ref, by = c("iso3c" = "ref_iso3c")) %>%
+    dplyr::mutate(Country = dplyr::coalesce(ref_country, Country))
 
   without_iso <- standardized %>%
     dplyr::filter(is.na(iso3c) | !nzchar(iso3c)) %>%
-    dplyr::left_join(country_ref, by = c("Country" = "country"))
+    dplyr::left_join(country_ref, by = c("Country" = "ref_country_std")) %>%
+    dplyr::mutate(
+      iso3c = dplyr::coalesce(iso3c, ref_iso3c),
+      Country = dplyr::coalesce(ref_country, Country)
+    )
 
-  dplyr::bind_rows(with_iso, without_iso) %>%
+  standardized_with_ref <- dplyr::bind_rows(with_iso, without_iso) %>%
+    dplyr::mutate(iso3c = toupper(as.character(iso3c))) %>%
+    dplyr::select(-dplyr::any_of(c("ref_iso3c", "ref_country", "ref_country_std")))
+
+  standardized_filtered <- standardized_with_ref %>%
     dplyr::filter(!is.na(iso3c), nzchar(iso3c))
+
+  if (nrow(standardized_filtered) == 0 && nrow(standardized_with_ref) > 0) {
+    warning(
+      "Country standardization dropped all rows in standardize_country_table(); ",
+      "returning rows without iso3c filtering."
+    )
+    return(standardized_with_ref)
+  }
+
+  standardized_filtered
 }
+
